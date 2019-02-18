7a76022da0b062288d826bfa2b9efcd7d6741094
Vy Ho <vyho0711@gmail.com>
2019-02-18 15:42:52 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+'''
+  
+  
+need:    
+    sample mean
+    sample standard deviation
+    sample size
+    mean from the claim
+    normal distribution
+    significance level
+    critical value
+    2 tailed test, left-tailed, or right-tailed?
+
+''''
+
+import pandas as pd
+
+# input collected data into a list
+array = [55,39,51,15,70,58,42,69,55,53,25,56,125,23,26,56,62,33,62,94,66,91,115,75,134,73,41,20,17,20,73,24,67,78,36,16]
+
+#convert the list to a dataframe
+
+df = pd.DataFrame(data=array)
+
+#use descriptive stat for sample mean & deviation
+df.describe()
